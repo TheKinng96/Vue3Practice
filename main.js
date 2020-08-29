1815f86bceb2b2953c4c8ee14221426fe7ca2f9c
TheKinng96 <fengyuan.yap@example.com>
2020-08-29 23:09:17 +0900

    added basic logic for cart management

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
       cart: 0,
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
-      inventory: 0,
+      inventory: 3,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
         { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
@@ -15,6 +15,7 @@ const app = Vue.createApp({
   methods: {
     addToCart() {
       this.cart++;
+      this.inventory--;
     },
     updateImage(variantImage) {
       this.image = variantImage;
